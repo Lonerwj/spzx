97255de6ea154a6c95259c3e24234d44ea797c46
lonerW <1104850148@qq.com>
2025-12-29 16:58:14 +0800

    feat(cart): 集成商品服务客户端
    - 添加 Feign 客户端配置以连接商品服务
    - 配置 EnableFeignClients 注解扫描商品服务包
    - 添加 service-product-client 依赖项
    - 添加 spring-cloud-openfeign-core 依赖项
    - 集成商品服务远程调用功能

--- spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/CartApplication.java
+++ spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/CartApplication.java
@@ -3,8 +3,12 @@ package com.atguigu.spzx.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableFeignClients(basePackages = {
+        "com.atguigu.spzx.feign.product"
+})
 public class CartApplication {
 
     public static void main(String[] args) {
